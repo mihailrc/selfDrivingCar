20e08b2295eeb58ac78c87e847853f9a6d662b99
Mihail Chirita <mihailrc@gmail.com>
2016-11-25 13:35:22 -0500

    Addressed review feedback take 2

--- trafficSigns/sdc/model.py
+++ trafficSigns/sdc/model.py
@@ -24,21 +24,22 @@ class Model():
             os.makedirs(directory)
         return directory
 
-    def evaluate(self, dataset, logits, session, type='Validation'):
+    def evaluate(self, dataset, logits, session, epoch=0):
         start_time = time.time()
         evals = np.ndarray(shape=(0, self.n_classes), dtype=np.float32)
         labels = dataset.labels
-        while dataset.epochs_completed == 0:
+        while dataset.epochs_completed == epoch:
             batch_images, batch_labels = dataset.next_batch(32, generate_image=False, shuffle_between_epochs=False)
             preds = session.run(logits, feed_dict=self.merge_dictionaries({self.x: batch_images, self.y: batch_labels},
                                                                           self.test_dict))
             evals = np.append(evals, preds, axis=0)
         correct_pred_2 = np.equal(np.argmax(evals, 1), np.argmax(labels, 1))
         testing_accuracy = np.sum(correct_pred_2) / dataset.num_examples
-        print(type + " accuracy {:.4f}%".format(testing_accuracy * 100))
+        return np.argmax(evals,1),  np.argmax(labels, 1), testing_accuracy
+        # print(type + " accuracy {:.4f}%".format(testing_accuracy * 100))
         # print("Calculated accuracy in {:.2f} seconds".format(time.time() - start_time))
 
-    def train(self, session, training_dataset, optimizer, cost, accuracy, summary_op,
+    def train(self, session, training_dataset, validation_dataset, testing_dataset, logits, optimizer, cost, accuracy, summary_op,
               training_epochs, batch_size=32, display_step=50, checkpoint_step=10, generate_image=True):
         start_time = time.time()
         summary_writer = tf.train.SummaryWriter(self.model_directory, session.graph)
@@ -65,10 +66,18 @@ class Model():
 
             if (training_dataset.epochs_completed % checkpoint_step == 0 and training_dataset.current_batch==0):
                 self.saver.save(session, self.checkpoints_dir + 'model', global_step=training_dataset.epochs_completed)
+            if (training_dataset.current_batch == 0):
+                epoch = training_dataset.epochs_completed-1
+                print('Summary for epoch {:d}'.format(epoch+1))
+                _,_,val_acc = self.evaluate(validation_dataset, logits,session, epoch=epoch)
+                print("Validation accuracy {:.4f}%".format(val_acc * 100))
+                _,_,test_acc = self.evaluate(testing_dataset, logits,session, epoch=epoch)
+                print("Test accuracy {:.4f}%".format(test_acc * 100))
+                # print("Total Training time = {:4.2f}".format(time.time() - start_time))
 
             step += 1
         end_time = time.time()
-        print("Training for {:d} epochs took {:.2f} seconds".format(training_epochs, end_time - start_time))
+        print("Trained {:d} epochs in {:.2f} seconds".format(training_epochs, end_time - start_time))
 
     def restore_model(self, path, session):
         self.saver.restore(sess=session, save_path=path)
